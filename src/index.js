3a393268d73236499e7f37debedb6480720728e8
Nikola Chobanov <chobbysan@gmail.com>
2022-12-19 11:16:39 +0200

    implemented working css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static('public'));
+//app.use(express.static('public')); v2
+
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+//app.set('views', './src/views')
 
 app.get('/', (req, res) => {
-	res.send('Hello world');
+	res.render('index');
 });
 
 app.listen(5000, () => console.log('App is listening on port 5000'));
