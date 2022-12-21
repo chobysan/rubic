36a74dd442513449d9f5af35f074487d37e16437
Nikola Chobanov <chobbysan@gmail.com>
2022-12-21 17:08:22 +0200

    add incremented id to created cubes

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,9 +3,9 @@ const cubes = require('../db.json');
 const path = require('path');
 
 exports.save = (cube) => {
-	cubes.push(cube);
+	cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube });
 	const data = JSON.stringify(cubes, '', 4);
 	return fs.writeFile(path.resolve('src', 'db.json'), data, { encoding: 'utf8' });
 };
 
-exports.getOne = (cubeId) => cubes[cubeId]
+exports.getOne = (cubeId) => cubes[cubeId];
