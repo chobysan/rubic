23a01c2d9f8d30afeab01206b39a6b4b16ed4315
Nikola Chobanov <chobbysan@gmail.com>
2022-12-21 19:30:16 +0200

    remember searchfields

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,7 +4,7 @@ const cubeService = require('../services/cubeService');
 router.get('/', (req, res) => {
 	let { search, from, to } = req.query;
 	const cubes = cubeService.getAll(search, from, to);
-	res.render('index', { cubes });
+	res.render('index', { cubes,search, from, to });
 });
 
 router.get('/about', (req, res) => res.render('about'));
